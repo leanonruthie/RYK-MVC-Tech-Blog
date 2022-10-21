7626aa1aa70b73d2f2aca1b74f6b925b2cb1f034
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-10-21 11:02:04 -0400

    about to start compartmentalized testing

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+// Work reference: RUT-VIRT-FSF-PT-06-2022-U-LOLC/14-MVC/01-Activities/28-Stu_Mini-Project
+module.exports = {
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    }
+  };
+  
