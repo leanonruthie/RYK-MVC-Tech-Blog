310141e030490a1f790528581880668f5b1affa1
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-10-21 15:12:23 -0400

    continually not picking up the public js files

--- public/logout.js
+++ public/logout.js
@@ -1,17 +1,15 @@
 // Work reference: RUT-VIRT-FSF-PT-06-2022-U-LOLC/14-MVC/01-Activities/28-Stu_Mini-Project
-
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,3 @@
-// Work reference: RUT-VIRT-FSF-PT-06-2022-U-LOLC/14-MVC/01-Activities/28-Stu_Mini-Project
 const sequelize = require('../config/connection');
 const { User, Story } = require('../models');
 
