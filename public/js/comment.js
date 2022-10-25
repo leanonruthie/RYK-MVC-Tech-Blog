f04b30826320e2023214c49e34ce1d9b4f8ef3c0
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-10-25 00:23:18 -0400

    still stuck on comments, must work on update story functionality next and must change cookie maxage to five hours

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,15 +1,14 @@
 // Work reference: RUT-VIRT-FSF-PT-06-2022-U-LOLC/14-MVC/01-Activities/28-Stu_Mini-Project
  // don't forget to add comment functionality!!!
 
- const commentFormHandler = async (event) => {
+ const newCommentHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#comment-title').value.trim();
   const description = document.querySelector('#comment').value.trim();
-  const id = event.target.getAttribute('data-id');
 
   if (name && description) {
-    const response = await fetch(`/api/comments/${id}`, {
+    const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({ name, description }),
       headers: {
@@ -25,7 +24,6 @@
   }
 };
 
-
-  document
-    .querySelector('#comment-form')
-    .addEventListener('submit', commentFormHandler);
+document
+  .querySelector('#comment-form')
+  .addEventListener('submit', newCommentHandler);
